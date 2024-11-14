88faf0b794cbc8cf380bad32ed32f0694ee9abab
kelvin2608 <kelvin@bvm.network>
2024-11-14 19:13:33 +0700

    Init AIPoweredWallet example
    (cherry picked from commit d44f5332b9894e1576a18d65fb1d13ff0a2532f5)

---
+++ examples/AIPoweredWallet/contracts/AIPoweredWallet.sol
@@ -0,0 +1,175 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.12;
+
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+
+interface AIKernel {
+    event NewInference(
+        uint256 indexed inferenceId,
+        address indexed model,
+        address indexed creator,
+        uint256 value,
+        uint256 originInferenceId
+    );
+
+    function infer(
+        bytes calldata _data,
+        bool _flag
+    ) external payable returns (uint256 referenceId);
+}
+
+interface PromptScheduler {
+    enum InferenceStatus {
+        Nil,
+        Solving,
+        Commit,
+        Reveal,
+        Processed,
+        Killed,
+        Transferred
+    }
+
+    enum AssignmentRole {
+        Nil,
+        Validating,
+        Mining
+    }
+
+    enum Vote {
+        Nil,
+        Disapproval,
+        Approval
+    }
+
+    struct Assignment {
+        uint256 inferenceId;
+        bytes32 commitment;
+        bytes32 digest;
+        uint40 revealNonce;
+        address worker;
+        AssignmentRole role;
+        Vote vote;
+        bytes output;
+    }
+
+    struct Inference {
+        uint256[] assignments;
+        bytes input;
+        uint256 value;
+        uint256 feeL2;
+        uint256 feeTreasury;
+        address modelAddress;
+        uint40 submitTimeout;
+        uint40 commitTimeout;
+        uint40 revealTimeout;
+        InferenceStatus status;
+        address creator;
+        address processedMiner;
+        address referrer;
+    }
+
+    function getInferenceInfo(
+        uint256 _inferenceId
+    ) external view returns (Inference memory);
+
+    function getAssignmentInfo(
+        uint256 _assignmentId
+    ) external view returns (Assignment memory);
+}
+
+contract AIPoweredWallet {
+    struct TxInfo {
+        address sender;
+        address receiver;
+        uint256 amount;
+    }
+
+    address public kernel;
+    address public promptScheduler;
+    mapping(address user => string) public context;
+    mapping(uint256 inferId => TxInfo) public txInfo;
+
+    event SuspiciousTransaction(uint256 indexed inferenceId, bytes prompt);
+    event Sent(uint256 indexed inferenceId, TxInfo txInfo);
+
+    constructor(address _kernelAddress, address _promptSchedulerAddress) {
+        require(
+            _kernelAddress != address(0) &&
+                _promptSchedulerAddress != address(0),
+            "AIPoweredWallet: Invalid address"
+        );
+        kernel = _kernelAddress;
+        promptScheduler = _promptSchedulerAddress;
+    }
+
+    function suspiciousTransaction(
+        address _receiver,
+        uint256 _amount
+    ) external {
+        string memory prompt = string.concat(
+            "Based on the following Ethereum transaction history, is there any indication of suspicious activity? Respond with only 'yes' or 'no'. ",
+            Strings.toHexString(msg.sender),
+            " transfer ",
+            Strings.toString(_amount),
+            " wei to ",
+            Strings.toHexString(_receiver),
+            ". ",
+            context[msg.sender]
+        );
+
+        uint256 inferenceId = AIKernel(kernel).infer(bytes(prompt), true);
+        txInfo[inferenceId] = TxInfo(msg.sender, _receiver, _amount);
+
+        emit SuspiciousTransaction(inferenceId, bytes(prompt));
+    }
+
+    function send(uint256 _inferenceId) external payable {
+        TxInfo memory info = txInfo[_inferenceId];
+
+        require(info.sender == msg.sender, "AIPoweredWallet: Unauthorized");
+
+        address receivedWallet = info.sender;
+        require(
+            receivedWallet != address(0),
+            "AIPoweredWallet: Invalid wallet address"
+        );
+        require(
+            info.amount == msg.value,
+            "AIPoweredWallet: Invalid transaction amount"
+        );
+
+        bytes memory result = fetchInferenceResult(_inferenceId);
+
+        require(
+            keccak256(result) == keccak256(abi.encodePacked("No")),
+            "AIPoweredWallet: Suspicious transaction"
+        );
+
+        payable(receivedWallet).transfer(msg.value);
+
+        context[msg.sender] = string.concat(
+            context[msg.sender],
+            Strings.toHexString(msg.sender),
+            " transfer ",
+            Strings.toString(msg.value),
+            " wei to ",
+            Strings.toHexString(receivedWallet),
+            ". "
+        );
+    }
+
+    function fetchInferenceResult(
+        uint256 _inferenceId
+    ) public view returns (bytes memory) {
+        PromptScheduler.Inference memory inferInfo = PromptScheduler(
+            promptScheduler
+        ).getInferenceInfo(_inferenceId);
+
+        if (inferInfo.assignments.length == 0) revert("Wait for inference");
+
+        return
+            PromptScheduler(promptScheduler)
+                .getAssignmentInfo(inferInfo.assignments[0])
+                .output;
+    }
+}
---
+++ examples/AIPoweredWallet/scripts/suspiciousTransaction.ts
@@ -0,0 +1,97 @@
+import { ethers, network, upgrades } from "hardhat";
+import { AIPoweredWallet } from "../typechain-types";
+import AIKernel from "../artifacts/contracts/AIPoweredWallet.sol/AIKernel.json";
+import { Address } from "hardhat-deploy/dist/types";
+import { assert } from "chai";
+
+const config = network.config as any;
+
+async function suspiciousTransaction() {
+  const networkName = network.name.toUpperCase();
+  const [sender, receiver] = await ethers.getSigners();
+
+  const aiPoweredWalletAddress = config.aiPoweredWallet;
+  assert(
+    aiPoweredWalletAddress,
+    `Missing ${networkName}_AI_POWERED_WALLET_ADDRESS from environment variables!`
+  );
+
+  const contractFactory = await ethers.getContractFactory("AIPoweredWallet");
+  const aiPoweredWallet = contractFactory.attach(
+    aiPoweredWalletAddress
+  ) as AIPoweredWallet;
+
+  const transferredAmt = ethers.parseEther("0.0001");
+
+  console.log(
+    `Check whether the transaction history is suspicious when sending ${ethers.formatEther(
+      transferredAmt
+    )} ETH from ${sender.address} to ${receiver.address}...`
+  );
+  const txCheck = await aiPoweredWallet
+    .connect(sender)
+    .suspiciousTransaction(receiver.address, transferredAmt);
+  const receipt = await txCheck.wait();
+
+  console.log("Tx hash: ", receipt?.hash);
+  console.log("Tx status: ", receipt?.status == 1 ? "Success" : "Failed");
+
+  let inferResult;
+  let inferId = 0;
+  if (receipt?.status == 1) {
+    // Get inference ID
+    inferId = getInferId(receipt)[0];
+    console.log("Inference ID: ", inferId);
+    console.log("Wait for inference result...");
+
+    // Wait for inference result
+    while (true) {
+      await sleep(30000);
+      try {
+        inferResult = await aiPoweredWallet.fetchInferenceResult(inferId);
+        break;
+      } catch (e: any) {
+        console.log(e.message.slice(":")[1]);
+        continue;
+      }
+    }
+  }
+
+  console.log("Inference result: ", ethers.toUtf8String(inferResult));
+  console.log("Send ETH after checking the transaction history...");
+
+  const txSend = await aiPoweredWallet.connect(sender).send(inferId, {
+    value: ethers.parseEther("0.0001"),
+  });
+  const receiptSend = await txSend.wait();
+  console.log("Tx hash: ", receiptSend?.hash);
+  console.log("Tx status: ", receiptSend?.status == 1 ? "Success" : "Failed");
+}
+
+export function getInferId(receipt: ethers.TransactionReceipt): number[] {
+  return receipt.logs
+    .filter(
+      (log: any) =>
+        log.topics[0] ===
+          ethers.id("NewInference(uint256,address,address,uint256,uint256)") &&
+        isAddressEq(log.address, config.promptSchedulerAddress)
+    )
+    .map((log: any) => {
+      return parseInt(log.topics[1], 16);
+    });
+}
+
+function isAddressEq(a: Address, b: Address): boolean {
+  return a.toLowerCase() === b.toLowerCase();
+}
+
+async function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+suspiciousTransaction()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
